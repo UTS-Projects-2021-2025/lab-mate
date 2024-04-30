a88f7b5d9e71b142883d14904f906e5c0fd5d596
Euan Mendoza <bebopbamf@effectfree.dev>
2024-04-30 12:38:56 +1000

    Initial Commit

---
+++ LabMate/LabMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabMateApp.swift
+//  LabMate
+//
+//  Created by Euan Mendoza on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct LabMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
