d5d5139e13362addd4d90ff2137c961ad7b2cb5a
CC0369 <98926546+CC0369@users.noreply.github.com>
2024-05-16 21:35:04 +1000

    Update LogoView.swift

--- LabMate/Views/LogoView.swift
+++ LabMate/Views/LogoView.swift
@@ -12,12 +12,12 @@ struct LogoView: View {
         HStack {
             Text("LabMate")
                 .foregroundColor(.black)
-                .font(.system(size: 50))
+                .font(.system(size: 40))
                 .fontWeight(.bold)
                 .padding(.top, 20)
             Image(.labMateLogo)
                 .resizable()
-                .frame(maxWidth: 80, maxHeight: 80)
+                .frame(maxWidth: 70, maxHeight: 70)
                 .padding(.top, 10)
                 .padding(.bottom, -15)
         }
